002d0774f7380da78b97e61d3477e636fa0c4bcc
kminhoo <whdgus4158@gmail.com>
2024-09-01 22:31:07 +0900

    feat: LululalaPiano 페이지 반응형(3)

--- src/components/sections/realization/PianoClass.tsx
+++ src/components/sections/realization/PianoClass.tsx
@@ -1,4 +1,5 @@
 import PianoTeacher from '@assets/icon/pianoTeacher.svg?react'
+import Up from '@assets/icon/up.svg?react'
 import class1 from '@assets/image/class1.jpg'
 import class2 from '@assets/image/class2.jpg'
 import class3 from '@assets/image/class3.jpg'
@@ -21,8 +22,8 @@ function PianoClass() {
             </div>
             <div className={styles.class__descContainer}>
               <p>룰루랄라 피아노</p>
-              <p>수업은 어떻게 이루어 질까요?</p>
-              <p>수업은 이렇게 이루어 집니다!</p>
+              <p>수업은 어떻게?</p>
+              <p>수업은 이렇게!</p>
             </div>
           </div>
         </div>
@@ -30,7 +31,7 @@ function PianoClass() {
         <div className={styles.class__classImgContainer}>
           <div>
             <div className={styles.class__activity}>
-              <h3 className={styles.class__activity__title}>Activite 연주</h3>
+              <h3 className={styles.class__activity__title}>Activity 연주</h3>
               <p className={styles.class__activity__subTitle}>
                 청음활동, 짝궁과 신체표현, 율동과 게임
               </p>
@@ -81,7 +82,7 @@ function PianoClass() {
                   src={class5}
                 />
                 <p className={styles.class__collaboration__desc}>
-                  핸드밸과 피아노의 콜라보 연주를 준비해요
+                  핸드볼과 피아노의 콜라보 연주를 준비해요
                 </p>
               </div>
               <div className={styles.class__collaboration__item}>
@@ -99,7 +100,7 @@ function PianoClass() {
                   src={class7}
                 />
                 <p className={styles.class__collaboration__desc}>
-                  친구들과 핸드폰을 이용해 화음을 만들어요
+                  친구들과 핸드볼을 이용해 화음을 만들어요
                 </p>
               </div>
             </div>
@@ -142,20 +143,57 @@ function PianoClass() {
 
         <div className={styles.class__textBook}>
           <div>
-            <div className={styles.class__container}>
-              <div className={styles.class__imgContainer}>
-                <PianoTeacher className={styles.class__imgContainer__img} />
+            <div className={styles.class__textBook__container}>
+              <div className={styles.class__textBook__imgContainer}>
+                <PianoTeacher
+                  className={styles.class__textBook__imgContainer__img}
+                />
               </div>
-              <div className={styles.class__descContainer}>
-                <p>피아노 교재</p>
-                <p>선생님~ 피아노 교재는 뭐에요?</p>
-                <p>
-                  룰루랄라 피아노 교재는 {/* //여기 수정해야할 차례 9월1일 */}
-                  <span className={styles.class__descContainer__highlight}>
-                    신.청.곡
-                  </span>
+              <div className={styles.class__textBook__descContainer}>
+                <p className={styles.class__textBook__firstDesc}>피아노 교재</p>
+                <p className={styles.class__textBook__secondDesc}>
+                  선생님~ 피아노 교재는 뭔가요?
+                </p>
+                <p className={styles.class__textBook__secondDesc}>
+                  룰루랄라 피아노 교재는
+                  <span>신.청.곡</span>
                 </p>
               </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.class__register}>
+          <div>
+            <div className={styles.class__register__flexContainer}>
+              <div className={styles.class__register__item}>
+                <p className={styles.class__register__text}>
+                  더 이상 반복되는 교재는
+                </p>
+                <p className={styles.class__register__no}>NO!</p>
+              </div>
+              <div className={styles.class__register__item}>
+                <p className={styles.class__register__text}>
+                  아이가 좋아하는 곡으로,
+                </p>
+                <p className={styles.class__register__text}>
+                  아이의 성취감도
+                  <Up className={styles.class__register__icon} />
+                </p>
+                <p className={styles.class__register__text}>
+                  아이의 만족도도
+                  <Up className={styles.class__register__icon} />
+                </p>
+              </div>
+            </div>
+            <div className={styles.class__register__btnContainer}>
+              <p className={styles.class__register__btnText}>
+                <span>신</span>나게, <span>청</span>해주렴,
+                <br /> 너희가 연주하고 싶은 <span>곡</span>
+              </p>
+              <button className={styles.class__register__btn}>
+                수업 신청하기
+              </button>
             </div>
           </div>
         </div>
