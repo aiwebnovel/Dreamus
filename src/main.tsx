617cd60adb0485d548aa3698cddfcc385697d257
권은비 <dev_eunbi@gwon-eunbiui-MacBookPro.local>
2024-10-15 14:23:51 +0900

    feat: 페이지 이동시 스크롤 위로 오게 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { BrowserRouter } from 'react-router-dom'
 
 import '@styles/global.scss'
 import { ModalContextProvider } from './context/ModalContext.tsx'
+import ScrollTop from './ScollTop.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ModalContextProvider>
       <BrowserRouter>
+        <ScrollTop />
         <App />
       </BrowserRouter>
     </ModalContextProvider>
